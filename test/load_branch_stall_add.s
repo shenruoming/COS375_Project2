7dcb63ff95c24e5a347c34a7d685714cfbedb7ea
Ruoming Shen <rs3270@princeton.edu>
2025-11-25 10:30:46 -0500

    test

---
+++ test/load_branch_stall_add.s
@@ -0,0 +1,13 @@
+addi a4, zero, 30
+addi a5, zero, 240
+sd a4, 0(a5)
+addi a4, a4, 1
+ld t5, 0(a5)
+addi t5, t5, 1
+beq t5, a4, branch
+addi t2, zero, 25
+
+branch:
+    addi t1, zero, 8
+
+.word 0xfeedfeed
