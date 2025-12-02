53a05c8254aedad5dfa8c16c4e9df9aa2fc96b87
Ruoming Shen <rs3270@princeton.edu>
2025-12-01 19:20:57 -0500

    test

--- src/cycle.cpp
+++ src/cycle.cpp
@@ -272,7 +272,7 @@ Status runCycles(uint64_t cycles) {
 // run till halt (call runCycles() with cycles == 1 each time) until
 // status tells you to HALT or ERROR out
 Status runTillHalt() {
-    uint64_t addresses[6] = {0x0, 0x4, 0x8, 0x0000F0011,  0x000FF0011, 0x0};
+    uint64_t addresses[6] = {0x0, 0x4, 0x8, 0x0000F0001,  0x000FF0001, 0x0};
     iCache->dump(output);
     for (int i = 0; i < 6; i++) {
         iCache->access(addresses[i], CACHE_READ);
