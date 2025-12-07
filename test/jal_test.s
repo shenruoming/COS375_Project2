3675c7ac173ebbf6e4eed4a8ee44c9debf93d278
Ruoming Shen <rs3270@princeton.edu>
2025-12-07 15:51:38 -0500

    jal tests

--- test/jal_test.s
+++ test/jal_test.s
@@ -2,12 +2,10 @@ addi a4, zero, 30
 addi a5, zero, 240
 sd a4, 0(a5)
 ld t5, 0(a5)
-jal t6, branch # change to jal
+jal t5\6, branch # change to jal
 addi t2, zero, 25
 
 branch:
-    addi t1, zero, 8
-    addi t1, zero, 8
     addi t1, zero, 8
 
 .word 0xfeedfeed
