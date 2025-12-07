1074f1b20396b7e73831893ee79cdec1edcec6d2
Ruoming Shen <rs3270@princeton.edu>
2025-12-07 16:00:02 -0500

    test

---
+++ test/jalr_test.s
@@ -0,0 +1,14 @@
+addi a4, zero, 30
+addi a5, zero, 240
+addi s3, zero, 8
+sd a4, 0(a5)
+ld t5, 0(a5)
+jalr t6, s3, branch # change to jal
+addi t2, zero, 25
+
+branch:
+    addi s4, zero, 1
+    addi s5, zero, 1
+    addi t1, zero, 8
+
+.word 0xfeedfeed
